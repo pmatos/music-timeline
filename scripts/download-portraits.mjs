8f04d0fecfe24f58f9311a63a23edd50949c7d28
Paulo Matos <p@ocmatos.com>
2026-07-17 10:23:47 +0200

    Add Leoš Janáček to piano timeline (#57)

--- scripts/download-portraits.mjs
+++ scripts/download-portraits.mjs
@@ -63,6 +63,7 @@ const WIKIPEDIA_TITLES = {
   'hindemith': 'Paul_Hindemith',
   'ibert': 'Jacques_Ibert',
   'cortot': 'Alfred_Cortot',
+  'janacek': 'Leoš_Janáček',
   'hofmann': 'Josef_Hofmann',
   'grainger': 'Percy_Grainger',
   'glenn-miller': 'Glenn_Miller',
