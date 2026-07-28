e179c578bb20dc00eeb48939bef9a9fbfa4e36a8
Paulo Matos <p@ocmatos.com>
2026-07-28 17:30:37 +0200

    feat(piano): add Bohuslav Martinů to piano timeline

--- scripts/download-portraits.mjs
+++ scripts/download-portraits.mjs
@@ -69,6 +69,7 @@ const WIKIPEDIA_TITLES = {
   'kid-ory': 'Kid_Ory',
   'hindemith': 'Paul_Hindemith',
   'ibert': 'Jacques_Ibert',
+  'martinu': 'Bohuslav_Martinů',
   'godowsky': 'Leopold_Godowsky',
   'price': 'Florence_Price',
   'cortot': 'Alfred_Cortot',
